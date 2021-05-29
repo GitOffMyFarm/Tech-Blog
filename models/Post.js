e8911a9ec1a09723fff17b6b79705b6df1b785cc
Joseph Freeman <freemanjj1992@gmail.com>
2021-05-29 17:16:06 -0400

    database seeds, info now in database

--- models/Post.js
+++ models/Post.js
@@ -16,7 +16,7 @@ Post.init(
             allowNull: false
         },
         body: {
-            type: DataTypes.STRING,
+            type: DataTypes.STRING(1000),
         },
         writer_id: {
             type: DataTypes.INTEGER,
