e1c8038f2280f6519bea1928704857a94d52ce58
Joseph Freeman <freemanjj1992@gmail.com>
2021-05-29 11:32:45 -0400

    added login text for HB and some routes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        res.render('homepage', {logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return
+    }
+
+res.render('login');
+})
